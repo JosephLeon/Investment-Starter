f1b10392b3e1483b11400583be721bef83108c88
Joseph Leon <joseph.leon.9@gmail.com>
2016-05-25 19:17:11 -0400

    Make the index method of the get_started controller to home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'get_started/index'
+  root 'get_started#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
